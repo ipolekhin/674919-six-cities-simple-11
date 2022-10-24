d11978d9a3629ad8aaafe6a6aeb34b27ad24335f
ipolekhin <ipolekhin@yandex.ru>
2022-10-24 23:08:58 +0300

    Маршрут по тайным тропам

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,9 +1,38 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {AppRoute} from '../../const';
 import Main from '../../pages/main/main';
+import Login from '../../pages/login/login';
+import Offer from '../../pages/offer/offer';
+import PageNotExist from '../../pages/page-not-exist/page-not-exist';
 
 type AppProps = {
   countPlaces: number;
 }
 
-const App = ({countPlaces}: AppProps): JSX.Element => <Main countPlaces={countPlaces}/>;
+const App = ({countPlaces}: AppProps): JSX.Element => (
+  <BrowserRouter>
+    <Routes>
+      <Route
+        path={AppRoute.Main}
+        element={<Main countPlaces={countPlaces}/>}
+      />
+
+      <Route
+        path={AppRoute.Login}
+        element={<Login/>}
+      />
+
+      <Route
+        path={AppRoute.Offer}
+        element={<Offer/>}
+      />
+
+      <Route
+        path='*'
+        element={<PageNotExist/>}
+      />
+    </Routes>
+  </BrowserRouter>
+);
 
 export default App;
