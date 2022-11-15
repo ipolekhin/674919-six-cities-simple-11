83eebe1b938172cd6df3d0d8752dc43c54c8ee85
ipolekhin <ipolekhin@yandex.ru>
2022-11-15 23:08:25 +0300

    Контроль и ограничения (часть 2)

--- project/src/store/action.ts
+++ project/src/store/action.ts
@@ -2,4 +2,7 @@ import {createAction} from '@reduxjs/toolkit';
 import {Offers} from '../types/offers';
 
 export const changeCity = createAction<string>('ChangeCity');
+
 export const setOffers = createAction<Offers>('SetOffers');
+
+export const setSortName = createAction<string>('SetSortName');
