1f36fa2647b948af7dffb409d3024fc7c916547e
ipolekhin <ipolekhin@yandex.ru>
2022-12-15 23:06:02 +0300

    snapshot test good

--- project/src/pages/page-not-exist/page-not-exist.snap.test.tsx
+++ project/src/pages/page-not-exist/page-not-exist.snap.test.tsx
@@ -2,15 +2,25 @@ import {render} from '@testing-library/react';
 import {createMemoryHistory} from 'history';
 import PageNotExist from './page-not-exist';
 import HistoryRoute from '../../components/history-route/history-route';
+import {Provider} from 'react-redux';
+import {configureMockStore} from '@jedmao/redux-mock-store';
+import {AuthorizationStatus} from '../../const';
 
 describe('Component: NotFoundScreen', () => {
+  const mockStore = configureMockStore();
+
   it('should render correctly', () => {
+    const store = mockStore({
+      USER: {authorizationStatus: AuthorizationStatus.NoAuth},
+    });
     const history = createMemoryHistory();
 
     const {container} = render(
-      <HistoryRoute history={history}>
-        <PageNotExist/>
-      </HistoryRoute>
+      <Provider store={store}>
+        <HistoryRoute history={history}>
+          <PageNotExist/>
+        </HistoryRoute>
+      </Provider>
     );
 
     expect(container).toMatchSnapshot();
